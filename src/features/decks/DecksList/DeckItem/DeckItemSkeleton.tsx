dd6b9ef96f8659f126879e485d92e89d8fb4069c
Starkov Sergey <starkovsr@gmail.com>
2024-01-04 11:47:52 +0300

    update DeckItemSkeleton component

--- src/features/decks/DecksList/DeckItem/DeckItemSkeleton.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItemSkeleton.tsx
@@ -1,23 +1,28 @@
 import s from './DeckItem.module.css'
 import Skeleton from 'react-loading-skeleton'
+import { ReactElement } from 'react'
 
-export const DeckItemSkeleton = ({ count }: { count: number }) => {
-  return Array(count)
-    .fill(null)
-    .map((_, index) => (
-      <li className={s.item} key={index}>
-        <h3 className={s.title}>
-          <Skeleton width={80} />
-        </h3>
-        <p className={s.characteristic}>
-          <Skeleton width={80} />
-        </p>
-        <p className={s.characteristic}>
-          <Skeleton width={200} />
-        </p>
-        <p className={s.characteristic}>
-          <Skeleton width={200} />
-        </p>
-      </li>
-    ))
+export const DeckItemSkeleton = ({ count }: { count: number }): ReactElement => {
+  return (
+    <>
+      {Array(count)
+        .fill(null)
+        .map((_, index) => (
+          <li className={s.item} key={index}>
+            <h3 className={s.title}>
+              <Skeleton width={80} />
+            </h3>
+            <p className={s.characteristic}>
+              <Skeleton width={80} />
+            </p>
+            <p className={s.characteristic}>
+              <Skeleton width={200} />
+            </p>
+            <p className={s.characteristic}>
+              <Skeleton width={200} />
+            </p>
+          </li>
+        ))}
+    </>
+  )
 }
